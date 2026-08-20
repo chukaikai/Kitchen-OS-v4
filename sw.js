ed34c2e1799db4e46264154f9d3cadef610b70c6
chukaikai <hpkailing@gmail.com>
2026-08-20 10:35:25 +0800

    ＫＩＴ

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kitchen-os-v41-central-ribeye-three-store-20260820-v1';
+const CACHE_NAME = 'kitchen-os-v41-central-ribeye-towel-three-store-20260820-v1';
 
 const ASSETS = [
   './',
